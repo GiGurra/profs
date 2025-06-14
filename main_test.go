9f43e220cd560e8c6369bacf05daa475c75379dd
Johan Kjölhede <gigurra@gmail.com>
2025-06-14 21:14:28 +0200

    wip on testability

--- main_test.go
+++ main_test.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/GiGurra/boa/pkg/boa"
 	"github.com/GiGurra/profs/internal"
+	"github.com/google/go-cmp/cmp"
+	"github.com/samber/lo"
 	"os"
+	"path/filepath"
 	"sync"
 	"testing"
 )
@@ -14,7 +17,7 @@ var testMutex = sync.Mutex{}
 // since we override global os.Args
 func runTest(
 	t *testing.T,
-	args []string,
+	argSet [][]string,
 	verifier func(t *testing.T, pan any, err error),
 ) {
 	testMutex.Lock()
@@ -36,7 +39,6 @@ func runTest(
 		}
 	}()
 
-	os.Args = args
 	var pan any = nil
 	var err error = nil
 	func() {
@@ -45,10 +47,13 @@ func runTest(
 				t.Errorf("Test panicked: %v", r)
 			}
 		}()
-		mainCmd().RunH(boa.ResultHandler{
-			Panic:   func(a any) { pan = a },
-			Failure: func(e error) { err = e },
-		})
+		for _, args := range argSet {
+			os.Args = args
+			mainCmd().RunH(boa.ResultHandler{
+				Panic:   func(a any) { pan = a },
+				Failure: func(e error) { err = e },
+			})
+		}
 		verifier(t, pan, err)
 	}()
 }
@@ -62,19 +67,93 @@ func checkNoFailures(t *testing.T, pan any, err error) {
 	}
 }
 
+func mkTempDir() string {
+	tempDir, err := os.MkdirTemp("", "profs-test")
+	if err != nil {
+		panic("Failed to create temp dir: " + err.Error())
+	}
+	return tempDir
+}
+
+func mkDir(pathParts ...string) string {
+	fullPath := filepath.Join(pathParts...)
+	err := os.MkdirAll(fullPath, 0755)
+	if err != nil {
+		panic("Failed to create dir: " + err.Error())
+	}
+	return fullPath
+}
+
+func deleteDirAndContents(dir string) {
+	if dir == "" {
+		return
+	}
+	err := os.RemoveAll(dir)
+	if err != nil {
+		panic("Failed to delete temp dir: " + err.Error())
+	}
+}
+
 func TestHelp(t *testing.T) {
-	runTest(t, []string{"profs", "--help"}, func(t *testing.T, pan any, err error) {
+	runTest(t, [][]string{{"profs", "--help"}}, func(t *testing.T, pan any, err error) {
 		checkNoFailures(t, pan, err)
 	})
 }
 
 func TestList(t *testing.T) {
-	runTest(t, []string{"profs", "list"}, func(t *testing.T, pan any, err error) {
+	runTest(t, [][]string{{"profs", "list"}}, func(t *testing.T, pan any, err error) {
 		checkNoFailures(t, pan, err)
 		// configs should be empty
 		conf := internal.LoadGlobalConf()
 		if conf.Paths != nil && len(conf.Paths) > 0 {
 			t.Fatalf("Expected no paths, got: %v", conf.Paths)
+		}
+	})
+}
+
+func TestAdd1(t *testing.T) {
+	testDir := mkTempDir()
+	defer func() { deleteDirAndContents(testDir) }()
+
+	dirToAdd := mkDir(testDir, "dir1")
+	runTest(t, [][]string{{"profs", "add", dirToAdd, "--profile", "test"}}, func(t *testing.T, pan any, err error) {
+		checkNoFailures(t, pan, err)
+
+		// Verify that the path was added
+		conf := internal.LoadGlobalConf()
+		if conf.Paths == nil || len(conf.Paths) != 1 {
+			t.Fatalf("Expected 1 path, got: %v", conf.Paths)
+		}
+
+		if conf.Paths[0].SrcPath != dirToAdd {
+			t.Fatalf("Expected path to be '%s', got: %s", dirToAdd, conf.Paths[0].SrcPath)
+		}
+	})
+}
+
+func TestAdd2(t *testing.T) {
+	testDir := mkTempDir()
+	defer func() { deleteDirAndContents(testDir) }()
+
+	dirToAdd1 := mkDir(testDir, "dir1")
+	dirToAdd2 := mkDir(testDir, "dir2")
+	runTest(t, [][]string{
+		{"profs", "add", dirToAdd1, "--profile", "test"},
+		{"profs", "add", dirToAdd2, "--profile", "test"},
+	}, func(t *testing.T, pan any, err error) {
+		checkNoFailures(t, pan, err)
+
+		// Verify that the path was added
+		conf := internal.LoadGlobalConf()
+		if len(conf.Paths) != 2 {
+			t.Fatalf("Expected 2 paths, got: %v", conf.Paths)
+		}
+
+		expected := []string{dirToAdd1, dirToAdd2}
+		if diff := cmp.Diff(lo.Map(conf.Paths, func(item internal.Path, _ int) string {
+			return item.SrcPath
+		}), expected); diff != "" {
+			t.Fatalf("Paths mismatch (-got +want):\n%s", diff)
 		}
 	})
 }
